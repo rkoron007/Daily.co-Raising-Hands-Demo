9199e50a6ec64a751999f131e0a78851262222b5
Rose <rkoron007@gmail.com>
2020-03-30 14:48:58 -0700

    Wrap api key import

--- client/index.js
+++ client/index.js
@@ -2,5 +2,9 @@ import * as APIFunctions from "./util/api-functions.js";
 window.APIFunctions = APIFunctions;
 import * as UIFunctions from "./util/ui-functions.js";
 window.UIFunctions = UIFunctions;
-import APIKEY from "./keys.js";
-window.APIKEY = APIKEY;
+
+// if we are hosting we'll set our API key directly
+if (!process.env["APIKEY"]) {
+  import APIKEY from "./keys.js";
+  window.APIKEY = APIKEY;
+}
